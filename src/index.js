404a8a11fe9f4863e198263cb202c65fb71dd544
annapik <anna.pikulskaya@yandex.ru>
2022-03-02 23:27:16 +0400

    feat: complete function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  let arr = [];
+  for( let i=0; i<str.length; i++){
+  let currentSymb = str.charAt(i);
+    for( let j=0; j<bracketsConfig.length; j++) {
+        if(currentSymb === bracketsConfig[j][0]){
+        if(arr[arr.length-1] === bracketsConfig[j][1]) {
+          arr.pop();
+        } else {
+          arr.push(currentSymb);
+        }
+       } else if(currentSymb === bracketsConfig[j][1]) {
+       if(arr.length > 0) {
+        if(arr[arr.length-1] === bracketsConfig[j][0]) {
+          arr.pop();
+        }
+       }
+      }
+    }
+  }
+ return arr.length === 0;
 }
+
